ee385d2754d60e06454cb3c95225c77ea590b3ec
Thomas Jollans <tjol@tjol.eu>
2017-05-06 19:39:54 +0200

    zsh dotfiles

---
+++ INSTALL/do-install.inc.zsh
@@ -0,0 +1,35 @@
+
+setopt extendedglob 
+
+installer_name=$(basename $0)
+
+# go to source directory (if not already there)
+cd $(dirname $(dirname "${0:a}"))
+
+echo INSTALLING dotfiles for $installer_name
+
+for myfile in ${(k)FILES}
+do
+    theirfile=${FILES[$myfile]}
+    
+    target="$BASE/$theirfile"
+
+    # check that the target file is not already a link to my file.
+    if [[ -h "$target" && "${myfile:A}" = "${target:A}" ]]; then
+        echo $target was already installed.
+    else
+        # Create directories as needed
+        mkdir -p $(dirname "$target") || (echo failed to install $target && exit 1) || exit 1
+
+        # Backup the target file if it existed before
+        if [[ -e "$target" ]]; then
+            mv "$target" "$target"~ || (echo failed to install $target && exit 1) || exit 1
+            echo created backup "$target"~
+        fi
+
+        # Do the deed
+        ln -v -s ${myfile:a} $target || (echo failed to install $target && exit 1) || exit 1
+    fi
+done
+
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# Thomas Jollans' dotfiles
+
+This is where I keep some *dotfiles*, that is to say some local configuration
+files for my Linux systems.
+
+## Installation
+
+Installation scripts are in `INSTALL/`; to install the zsh files, run
+`INSTALL/zsh` and so on.
+
+## License
+
+The help files included in zsh/help/ and the script zsh/helpfiles.pl are
+taken from the [ZSH](http://www.zsh.org/) project. Please find the copyright
+notice and licence for these files in `LICENSE_zsh`.
+
+The remaining files may be used, modified and distributed under the terms of
+the MIT licence as included in the file `LICENCE`
+
+
